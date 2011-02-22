75ffd4fdbe510a6b5fd217d56670c7e57c2273bb
Yoshiaki Kuwata <kuwata@gmail.com>
2011-02-21 22:07:22 -0800

    added Class files

---
+++ test_window_slider/Classes/test_window_sliderAppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  test_window_sliderAppDelegate.h
+//  test_window_slider
+//
+//  Created by Yoshiaki Kuwata on 2/20/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface test_window_sliderAppDelegate : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
+
