289cf750da425c7d60f4a0da516a9d5aab173ccd
Yoshiaki Kuwata <kuwata@gmail.com>
2011-05-08 14:26:24 -0700

    plain project

---
+++ test_sensors/Classes/test_sensorsAppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  test_sensorsAppDelegate.h
+//  test_sensors
+//
+//  Created by Yoshiaki Kuwata on 5/8/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface test_sensorsAppDelegate : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
+
